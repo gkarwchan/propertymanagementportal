f936e8332ccad1fde0255dbeffff67ac46a9248e
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2024-03-16 14:20:39 -0600

    add blob

---
+++ src/PropertyManagement.Storage/BlobUtil.cs
@@ -0,0 +1,8 @@
+public class BlobUtil
+{
+    public async Task<string> GetClient()
+    {
+        var r = new Uri("DefaultEndpointsProtocol=https;AccountName=gkartestcode;AccountKey=rJq8758T9LE3uholjzkoKUMhxrRzVgT3ipsJ1cfqGlvPU66xhZ+OzQxd5BdKzzLT0K70gWEvJxn++ASt2mXD3g==;EndpointSuffix=core.windows.net");
+        return "";
+    }
+}
