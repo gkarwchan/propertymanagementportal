a955f1cf7d3cd8247e9ad938a44dfb7cab9a1c2c
Ghassan Karwchan <ghassan.karwchan@gmail.com>
2024-07-29 18:46:16 -0600

    add event hub

---
+++ src/EventHubSender/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+int numOfEvents = 3;
+
+EventHubProducerClient producerClient = new EventHubProducerClient("", "");
+using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+for (int i = 1; i < numOfEvents; i++)
+{
+    if (!eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}"))))
+    {
+        throw new Exception($"Event {i} is tool large to batch and cannot be sent");
+    }
+}
+
+try
+{
+    await producerClient.SendAsync(eventBatch);
+    Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+    Console.ReadLine();
+}
+finally
+{
+    await producerClient.DisposeAsync();
+}
